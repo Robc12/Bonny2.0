02078884febf636031de4e8318bfc5c8ed93a6d9
Robc12 <mr_coolsport@hotmail.com>
2012-12-18 13:25:56 +0100

    Readme
    Readme

--- src/CCIndex/index.tpl.php
+++ src/CCIndex/index.tpl.php
@@ -1,21 +1,21 @@
 <h1>Index Controller</h1>
-<p>Welcome to Lydia index controller.</p>
+<p>Welcome to Bonny index controller.</p>
 
 <h2>Download</h2>
-<p>You can download Lydia from github.</p>
+<p>You can download Bonny from github.</p>
 <blockquote>
-<code>git clone git://github.com/mosbth/lydia.git</code>
+<code>https://github.com/Robc12/Bonny2.0.git</code>
 </blockquote>
-<p>You can review its source directly on github: <a href='https://github.com/mosbth/lydia'>https://github.com/mosbth/lydia</a></p>
+<p>You can review its source directly on github: <a href='https://github.com/Robc12/Bonny2.0'>https://github.com/Robc12/Bonny</a></p>
 
 <h2>Installation</h2>
-<p>First you have to make the data-directory writable. This is the place where Lydia needs
+<p>First you have to make the data-directory writable. This is the place where Bonny needs
 to be able to write and create files.</p>
 <blockquote>
-<code>cd lydia; chmod 777 site/data</code>
+<code>cd Bonny; chmod 777 site/data</code>
 </blockquote>
 
-<p>Second, Lydia has some modules that need to be initialised. You can do this through a 
+<p>Second, Bonny has some modules that need to be initialised. You can do this through a 
 controller. Point your browser to the following link.</p>
 <blockquote>
 <a href='<?=create_url('module/install')?>'>module/install</a>
